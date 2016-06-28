c97573ba28f27cabff648471c981fb50f544a46f
Yvan Sraka <yvan.sraka@gmail.com>
2016-06-28 11:24:49 +0200

    Update index-skeleton.php

--- index-skeleton.php
+++ index-skeleton.php
@@ -63,8 +63,6 @@ if ($_POST) {
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/3.0.2/firebase.js"></script>
-    <script src="./script.js"></script>
     <!-- Material Design Light -->
     <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
 </body>
