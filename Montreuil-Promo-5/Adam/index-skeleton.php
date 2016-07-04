e77c8d8501f396655531b7a1a11efc0f427e8cb2
Adam Sakhr <adam.sakhr@gmail.com>
2016-07-04 12:29:23 +0200

    Update index-skeleton.php

--- Montreuil-Promo-5/Adam/index-skeleton.php
+++ Montreuil-Promo-5/Adam/index-skeleton.php
@@ -137,9 +137,7 @@ if ($_POST) {
         assetsPath: 'lib/img/',
         popupButtonClasses: 'fa fa-smile-o'
       });
-      // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
-      // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
-      // It can be called as many times as necessary; previously converted input fields will not be converted again
+      
       window.emojiPicker.discover();
     });
   </script>
